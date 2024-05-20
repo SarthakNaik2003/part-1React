95ffbf0edf2ce956b3a237344ce96e6579046e58
SarthakNaik2003 <131641741+SarthakNaik2003@users.noreply.github.com>
2024-05-20 17:45:41 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,60 @@
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    { name: 'Fundamentals of React', exercises: 10 },
-    { name: 'Using props to pass data', exercises: 7 },
-    { name: 'State of a component', exercises: 14 }
-  ];
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  };
 
   const Header = () => {
     return (
-      <h1>{course}</h1>
-    )
-  }
+      <h1>{course.name}</h1>
+    );
+  };
 
   const Content = () => {
     return (
       <div>
-        {parts.map((part, index) => (
+        {course.parts.map((part, index) => (
           <Part key={index} name={part.name} exercises={part.exercises} />
         ))}
       </div>
-    )
-  }
+    );
+  };
 
   const Part = (props) => {
     return (
       <p>
         {props.name} {props.exercises}
       </p>
-    )
-
-  }
-
+    );
+  };
 
   const Total = () => {
+    const totalExercises = course.parts.reduce((total, part) => total + part.exercises, 0);
     return (
-      <p>Number of exercises {parts.exercises}</p>
-    )
-  }
-
+      <p>Number of exercises {totalExercises}</p>
+    );
+  };
 
   return (
-
     <div>
-      <Header course={course} />
+      <Header />
       <Content />
       <Total />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
