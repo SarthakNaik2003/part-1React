aaa552b7cbc03362253e19aa5c105916945d7ea1
SarthakNaik2003 <131641741+SarthakNaik2003@users.noreply.github.com>
2024-05-21 11:11:41 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,104 @@
-const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  };
+import { useState } from "react";
 
-  const Header = () => {
-    return (
-      <h1>{course.name}</h1>
-    );
-  };
-
-  const Content = () => {
+const History = (props) => {
+  if (props.allClicks.length === 0) {
     return (
       <div>
-        {course.parts.map((part, index) => (
-          <Part key={index} name={part.name} exercises={part.exercises} />
-        ))}
+        the app is used by pressing the buttons
       </div>
-    );
-  };
+    )
+  }
+  return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
 
-  const Part = (props) => {
-    return (
-      <p>
-        {props.name} {props.exercises}
-      </p>
-    );
-  };
+const App = () => {
+  // const course = {
+  //   name: 'Half Stack application development',
+  //   parts: [
+  //     {
+  //       name: 'Fundamentals of React',
+  //       exercises: 10
+  //     },
+  //     {
+  //       name: 'Using props to pass data',
+  //       exercises: 7
+  //     },
+  //     {
+  //       name: 'State of a component',
+  //       exercises: 14
+  //     }
+  //   ]
+  // };
 
-  const Total = () => {
-    const totalExercises = course.parts.reduce((total, part) => total + part.exercises, 0);
-    return (
-      <p>Number of exercises {totalExercises}</p>
-    );
-  };
+  // const Header = () => {
+  //   return (
+  //     <h1>{course.name}</h1>
+  //   );
+  // };
+
+  // const Content = () => {
+  //   return (
+  //     <div>
+  //       {course.parts.map((part, index) => (
+  //         <Part key={index} name={part.name} exercises={part.exercises} />
+  //       ))}
+  //     </div>
+  //   );
+  // };
+
+  // const Part = (props) => {
+  //   return (
+  //     <p>
+  //       {props.name} {props.exercises}
+  //     </p>
+  //   );
+  // };
+
+  // const Total = () => {
+  //   const totalExercises = course.parts.reduce((total, part) => total + part.exercises, 0);
+  //   return (
+  //     <p>Number of exercises {totalExercises}</p>
+  //   );
+  // };
+
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClick, setAll] = useState([])
+  const [total, setTotal] = useState(0, 1)
+
+  const handleLeftClick = () => {
+    setAll(allClick.concat('L'))
+    const updatedLeft = left + 1
+    setLeft(updatedLeft)
+    setTotal(updatedLeft + right)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClick.concat('R'))
+    const updatedRight = right + 1
+    setRight(updatedRight)
+    setTotal(left + updatedRight)
+  }
+
 
   return (
     <div>
-      <Header />
+      {left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {right}
+      <p>{allClick.join(" ")}</p>
+
+      <h2>{total}</h2>
+
+
+      {/* <Header />
       <Content />
-      <Total />
+      <Total /> */}
     </div>
   );
 };
